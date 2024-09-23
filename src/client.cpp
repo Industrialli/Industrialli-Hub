1335e76a93ac849ac0de9400db8e8653e75a3ad8
opallace <wmielniczki15@gmail.com>
2024-09-23 15:02:36 -0300

    modbus tcp complete

--- src/client.cpp
+++ src/client.cpp
@@ -2,12 +2,19 @@
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
 
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "opallace"
+#define AIO_KEY         "aio_DQmb66ocrc9ceB3hXe9CVff1e7Sx"
 
 industrialli_hub hub;
 EthernetClient client_adafruit;
 EthernetClient client;
 
-
+Adafruit_MQTT_Client mqtt(&client_adafruit, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish hz = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/hzz");
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/temperature");
+Adafruit_MQTT_Publish vibration = Adafruit_MQTT_Publish(&mqtt,  AIO_USERNAME "/feeds/vibration");
 
 uint8_t mac[]  = {0xDE, 0xAD, 0xBE, 0x68, 0x68, 0x6C};
 
@@ -30,19 +37,19 @@ void setup(){
 void loop(){
     if(mqtt.connected()){
         modbus_client.write_single_register(121, 100);
-        delay(5000);
+        delay(10000);
 
         modbus_client.read_holding_registers(121, 1);
         hz.publish((double)modbus_client.get_holding_register(121)/10.0);
         
         modbus_client.write_single_register(121, 300);
-        delay(5000);
+        delay(10000);
         
         modbus_client.read_holding_registers(121, 1);
         hz.publish((double)modbus_client.get_holding_register(121)/10.0);
         
         modbus_client.write_single_register(121, 600);
-        delay(5000);
+        delay(10000);
 
         modbus_client.read_holding_registers(121, 1);
         hz.publish((double)modbus_client.get_holding_register(121)/10.0);
